7ef1df6c6dc7b76b3a1be0190e8fc833a8bf8615
rhaneyko <rhaneykohonorio@gmail.com>
2024-06-11 20:58:20 -0400

    Adicionar arquivos de projeto.

---
+++ catalogo-de-filme/Controllers/SeriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace catalogo_de_filme.Controllers
+{
+    public class SeriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ catalogo-de-filme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace catalogo_de_filme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
